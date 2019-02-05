25c628eb1c4ae1b9e86242cd2230598de29d40c3
kidman90 <kacprzak90@gmail.com>
2019-02-05 12:11:49 +0100

    04 Handle Errors in Asynchronous Functions

---
+++ async04.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+async function fetchGitHubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const body = await response.json();
+
+  if (response.status !== 200)
+    throw Error(body.message);
+
+  return body;
+}
+
+// fetchGitHubUser('ireallydonotexist')
+//   .then(user => {
+//     console.log(user);
+//   })
+//   .catch(err => console.log(`Error: ${err.message}`));
+
+async function showGitHubUser(handle) {
+  try {
+    const user = await fetchGitHubUser(handle);
+    console.log(user.name);
+    console.log(user.location);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+  }
+}
+
+showGitHubUser('ireallydonotexist');
